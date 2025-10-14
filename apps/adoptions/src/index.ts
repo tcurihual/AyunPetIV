a4888f41c85abee30ae43b83a4ebc884469ee571
Huenuman <chuenumanj@gmail.com>
2025-10-13 22:51:44 -0300

    feat: :art: Se crea contexto de publicaciones en mobile

--- apps/adoptions/src/index.ts
+++ apps/adoptions/src/index.ts
@@ -7,6 +7,7 @@ import { errorHandler, ADOPTIONS_PORT, getHeaders, createSupabaseClient } from "
 import mineRequestRoutes from "./routes/requests"
 import reportRoutes from "./routes/reports"
 import messagesRoutes from "./routes/messages"
+import postsRoutes from "./routes/posts"
 
 export const supabase = createSupabaseClient()
 
@@ -22,6 +23,7 @@ app.use(express.urlencoded({ extended: true }))
 app.use("/", mineRequestRoutes)
 app.use("/reports", reportRoutes)
 app.use("/messages", messagesRoutes)
+app.use("/", postsRoutes)
 
 app.get("/", (_, res) => {
     return res.status(200).json({
